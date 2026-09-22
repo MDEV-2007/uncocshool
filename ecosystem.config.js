791d7f44e96ab706c7f013b2c2c39e0c1f79096f
MDEV-2007 <uzbekcoders0706@gmail.com>
2026-09-22 22:47:41 +0500

    Use serve for static export with PM2 on port 3001

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,14 +2,14 @@ module.exports = {
   apps: [
     {
       name: 'uncoschool',
-      script: 'node_modules/next/dist/bin/next',
-      args: 'start -p 3001 -H 0.0.0.0',
+      script: 'node_modules/serve/build/main.js',
+      args: 'out -l 3001 -s',
       cwd: './',
       exec_mode: 'fork',
       instances: 1,
       autorestart: true,
       watch: false,
-      max_memory_restart: '800M',
+      max_memory_restart: '300M',
       env: {
         NODE_ENV: 'production'
       }
